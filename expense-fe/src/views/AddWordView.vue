a5a66b9495a96c66626e8cd7600e856970fce4d0
Vivfsymr <vivfsymr.evol@gmail.com>
2025-09-15 14:26:35 +0700

    Implement input escaping and clipboard functionality in AddWordView

--- expense-fe/src/views/AddWordView.vue
+++ expense-fe/src/views/AddWordView.vue
@@ -19,9 +19,53 @@
     <div v-if="success" class="success-msg">Đã lưu thành công!</div>
     <div v-if="error" class="error-msg">{{ error }}</div>
   </div>
-</template>
-</script>
 
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+import axios from 'axios'
+
+const input = ref('')
+const escaped = ref('')
+const loading = ref(false)
+const success = ref(false)
+const error = ref('')
+
+function escapeString(str) {
+  return str
+    .replace(/\\/g, '\\\\')
+    .replace(/\n/g, '\\n')
+    .replace(/"/g, '\\"')
+}
+
+function escapeAndCopy() {
+  escaped.value = escapeString(input.value)
+  if (escaped.value) {
+    navigator.clipboard.writeText(escaped.value)
+  }
+}
+
+watch(input, escapeAndCopy)
+
+async function submit() {
+  loading.value = true
+  success.value = false
+  error.value = ''
+  try {
+    const formData = new FormData()
+    formData.append('body', escaped.value)
+    await axios.post('https://expense-be-dwhg.onrender.com/api/words/form', formData)
+    success.value = true
+    input.value = ''
+    escaped.value = ''
+  } catch (e) {
+    error.value = e?.response?.data?.message || 'Có lỗi xảy ra!'
+  } finally {
+    loading.value = false
+  }
+}
+</script>
 <style scoped>
 .add-word-dark {
   max-width: 800px;
